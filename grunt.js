a4dc6a79742c0bca457009b4915485e100d5a6a0
Olivier Audard <audard@gmail.com>
2012-07-15 19:29:37 +0200

    initial commit

---
+++ grunt.js
@@ -0,0 +1,74 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Loads grunt-less task
+  // see: https://github.com/jharding/grunt-less
+  grunt.loadNpmTasks('grunt-less');
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    meta: {
+      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+    },
+    lint: {
+      files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },/*
+    concat: {
+      dist: {
+        src: ['<banner:meta.banner>', '<file_strip_banner:lib/<%= pkg.name %>.js>'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },*/
+    less: {
+      all: {
+        src: 'less/style.less',
+        dest: 'less/style.css',
+        options: {
+          compress: true
+        }
+      }
+    },
+    watch: {
+      files: ['<config:lint.files>', '<config:less.all.src>'],
+      tasks: 'lint qunit less'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        jQuery: true
+      }
+    },
+    uglify: {}
+  });
+
+  // Default task.
+  // grunt.registerTask('default', 'lint qunit concat min less');
+  grunt.registerTask('default', 'lint qunit less');
+
+};
